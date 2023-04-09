66862ca81bec4368211dbbeb8c83e7739a56cc3f
Ale <alejandrabotacura@outlook.com.ar>
2023-04-09 11:06:01 -0300

    Added button effect, access to the store, currently working

--- src/components/Centerdiv.jsx
+++ src/components/Centerdiv.jsx
@@ -1,11 +1,36 @@
 import { PhraseGen } from "./PhraseGen";
+import { useEffect, useState } from "react";
+import { fetchAllPhrases } from "../store/slices/phrases";
+import { setpayload } from "../store/slices/fetch";
+import { useDispatch, useSelector } from "react-redux";
 export function Centerdiv() {
+  const fetch = useSelector((state) => state.fetchReducer.fetchStatus);
+  const dispatch = useDispatch();
+  const [buttonState, setButtonState] = useState(true);
+
+  const changePhrase = () => {
+    setButtonState(true);
+    dispatch(setpayload(false));
+    dispatch(fetchAllPhrases());
+  };
+  useEffect(() => {
+    if (fetch === true) {
+      setButtonState(false);
+    }
+  }, [fetch]);
+
   return (
     <>
       <div className="centerdiv">
         <PhraseGen />
       </div>
-      <button className="reloadbutton">Push me!</button>
+      <button
+        className="reloadbutton"
+        onClick={changePhrase}
+        disabled={buttonState}
+      >
+        Push me!
+      </button>
     </>
   );
 }
