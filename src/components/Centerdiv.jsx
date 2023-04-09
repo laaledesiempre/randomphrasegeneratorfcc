c808e13ef49c9d153481f6c404091acd356e596a
Ale <alejandrabotacura@outlook.com.ar>
2023-04-09 20:03:45 -0300

    Color interaction and Jquery added, i didnt tested more bcs of i exceed the API limits, everything seems to work but have to test it.

--- src/components/Centerdiv.jsx
+++ src/components/Centerdiv.jsx
@@ -1,4 +1,5 @@
 import { PhraseGen } from "./PhraseGen";
+import $ from "jquery";
 import { useEffect, useState } from "react";
 import { fetchAllPhrases } from "../store/slices/phrases";
 import { setpayload } from "../store/slices/fetch";
@@ -7,7 +8,10 @@ export function Centerdiv() {
   const fetch = useSelector((state) => state.fetchReducer.fetchStatus);
   const dispatch = useDispatch();
   const [buttonState, setButtonState] = useState(true);
-
+  const classArray = {
+    currentclassname: "",
+    palletes: ["pastelpink", "twilightpallete", "deepgreen"],
+  };
   const changePhrase = () => {
     setButtonState(true);
     dispatch(setpayload(false));
@@ -16,6 +20,11 @@ export function Centerdiv() {
   useEffect(() => {
     if (fetch === true) {
       setButtonState(false);
+      classArray.currentclassname = classArray.palletes.filter(
+        (e) => e !== classArray.currentclassname
+      )[Math.floor(Math.random() * (classArray.palletes.length - 1))];
+
+      $("body").attr("class", classArray.currentclassname);
     }
   }, [fetch]);
 
